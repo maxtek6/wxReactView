f554591644f688b5d6f820f812edf1210fec28bc
John R. Patek <johnpatek2@gmail.com>
2025-06-05 15:18:20 -0500

    save point

--- wxreactview.cpp
+++ wxreactview.cpp
@@ -1,6 +1,201 @@
+/*
+ * Copyright (c) 2025 Maxtek Consulting
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <wxreactview.hpp>
+#include <hyperpage.hpp>
 
-class wxReactViewHandler : public wxWebViewHandler
+#include <unordered_map>
+
+#include <wx/uri.h>
+#include <wx/mstream.h>
+
+#ifndef __WXMSW__
+#include <wx/webview_chromium.h>
+#endif
+
+static wxString normalizePath(const wxString &path)
 {
+    wxString normalizedPath = path;
+    if (!normalizedPath.StartsWith("/"))
+    {
+        normalizedPath.Prepend("/");
+    }
+    return normalizedPath;
+}
 
+class HyperpageResponseData : public wxWebViewHandlerResponseData
+{
+public:
+    HyperpageResponseData(const wxSharedPtr<hyperpage::page> &page)
+        : m_stream(new wxMemoryInputStream(page->get_content(), page->get_length()))
+    {
+    }
+
+    virtual ~HyperpageResponseData() = default;
+
+    wxInputStream *GetStream() override
+    {
+        return m_stream.get();
+    }
+
+private:
+    std::unique_ptr<wxMemoryInputStream> m_stream;
 };
+
+class HyperpageContent : public wxReactContent
+{
+public:
+    HyperpageContent(std::unique_ptr<hyperpage::page> &page) : m_page(page.release())
+    {
+        m_responseData = wxSharedPtr<wxWebViewHandlerResponseData>(new HyperpageResponseData(m_page));
+    }
+
+    virtual ~HyperpageContent() = default;
+
+    wxString GetContentType() const override
+    {
+        return wxString::FromUTF8(m_page->get_mime_type());
+    }
+
+    wxSharedPtr<wxWebViewHandlerResponseData> GetContent() override
+    {
+        return m_responseData;
+    }
+
+private:
+    wxSharedPtr<hyperpage::page> m_page;
+    wxSharedPtr<wxWebViewHandlerResponseData> m_responseData;
+};
+
+class HyperpageArchive : public wxReactArchive
+{
+public:
+    HyperpageArchive(const wxString &dbPath) : m_reader(dbPath.ToStdString())
+    {
+    }
+
+    wxSharedPtr<wxReactContent> LoadContent(const wxString &path) override
+    {
+        std::unique_ptr<hyperpage::page> page = m_reader.load(path.ToStdString());
+        wxSharedPtr<wxReactContent> content;
+        if (page)
+        {
+            return wxSharedPtr<wxReactContent>(new HyperpageContent(page));
+        }
+        return content;
+    }
+
+private:
+    hyperpage::reader m_reader;
+};
+
+wxSharedPtr<wxReactArchive> wxReactArchive::CreateHyperpageArchive(const wxString &dbPath)
+{
+    return wxSharedPtr<wxReactArchive>(new HyperpageArchive(dbPath));
+}
+
+wxReactHandler::wxReactHandler(const wxSharedPtr<wxReactArchive> &archive)
+    : wxWebViewHandler("https"), m_archive(archive)
+{
+    SetVirtualHost("wxreactview.ipc");
+}
+
+void wxReactHandler::StartRequest(const wxWebViewHandlerRequest &request,
+                                  wxSharedPtr<wxWebViewHandlerResponse> response)
+{
+    std::cerr << "Handling request for URI: " << request.GetURI() << std::endl;
+    const wxURI wxUri(request.GetURI());
+    const wxString &path = normalizePath(wxUri.GetPath());
+    if (path.StartsWith("/api/"))
+    {
+        auto it = m_requestHandlers.find(path);
+        if (it != m_requestHandlers.end())
+        {
+            it->second(request, response);
+        }
+        else
+        {
+            response->SetStatus(404);
+            response->FinishWithError();
+        }
+    }
+    else
+    {
+        auto content = m_archive->LoadContent(path);
+        if (content)
+        {
+            std::cerr << "Loaded content for path: " << path << std::endl;
+            response->SetStatus(200);
+            response->SetContentType(content->GetContentType());
+            response->Finish(content->GetContent());
+        }
+        else
+        {
+            std::cerr << "Content not found for path: " << path << std::endl;
+            response->SetStatus(404);
+            response->FinishWithError();
+        }
+    }
+}
+
+void wxReactHandler::RegisterEndpoint(const wxString &endpoint, const wxRequestHandler &handler)
+{
+    wxString key("/api" + endpoint);
+    m_requestHandlers[endpoint] = handler;
+}
+
+bool wxReactApp::StartProcess()
+{
+    // This function can be used to start the process if needed.
+    // For example, you might want to initialize some resources or set up IPC.
+    return true;
+}
+
+bool wxReactApp::OnInit()
+{
+    return StartProcess() && OnReady();
+}
+
+wxReactView::wxReactView(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size, long style, const wxString &name)
+{
+#ifdef __WXMSW__
+    const wxString backend(wxWebViewBackendEdge);
+#else
+    const wxString backend(wxWebViewBackendChromium);
+#endif
+    m_webView.reset(wxWebView::New(parent, id, "", pos, size, backend, style, name));
+}
+
+void wxReactView::Initialize(wxReactHandler *handler, const wxString &indexPage)
+{
+    std::cerr << "Initializing wxReactView with handler and index page: " << indexPage << std::endl;
+    m_handler = wxSharedPtr<wxWebViewHandler>(handler);
+    m_indexPage = indexPage;
+    m_webView->Bind(
+        wxEVT_WEBVIEW_CREATED,
+        [this](wxWebViewEvent &event)
+        {
+            std::cerr << "WebView created, registering handler and loading index page." << std::endl;
+            m_webView->RegisterHandler(m_handler);
+            m_webView->LoadURL(wxString::Format("https://wxreactview.ipc/%s", m_indexPage));
+        });
+}
--- wxreactview.hpp
+++ wxreactview.hpp
@@ -1,28 +1,60 @@
+/*
+ * Copyright (c) 2025 Maxtek Consulting
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef WXREACTVIEW_HPP
+#define WXREACTVIEW_HPP
+
 #include <wx/wx.h>
 #include <wx/filename.h>
 #include <wx/webview.h>
 
-class wxReactView
+using wxRequestHandler = std::function<void(const wxWebViewHandlerRequest &, wxSharedPtr<wxWebViewHandlerResponse>)>;
+
+class wxReactContent
 {
 public:
-    wxReactView(wxWindow *parent,
-                wxWindowID id,
-                const wxPoint &pos = wxDefaultPosition,
-                const wxSize &size = wxDefaultSize,
-                long style = 0,
-                const wxString &name = wxASCII_STR(wxWebViewNameStr));
-    
-    void Initialize(const wxString& directoryMapping);
+    virtual ~wxReactContent() = default;
+    virtual wxString GetContentType() const = 0;
+    virtual wxSharedPtr<wxWebViewHandlerResponseData> GetContent() = 0;
+};
 
-    void Send(const wxString& message);
+class wxReactArchive
+{
+public:
+    static wxSharedPtr<wxReactArchive> CreateHyperpageArchive(const wxString &dbPath);
+    virtual wxSharedPtr<wxReactContent> LoadContent(const wxString &path) = 0;
+};
 
+class wxReactHandler : public wxWebViewHandler
+{
+public:
+    wxReactHandler(const wxSharedPtr<wxReactArchive> &archive);
+    virtual ~wxReactHandler() = default;
+    void StartRequest(const wxWebViewHandlerRequest &request,
+                      wxSharedPtr<wxWebViewHandlerResponse> response) override final;
+    void RegisterEndpoint(const wxString &endpoint, const wxRequestHandler &handler);
 private:
-    static wxString GetWebviewBackend();
-    void OnWebViewCreated(wxWebViewEvent& event);
-    void OnWebViewScriptMessageReceived(wxWebViewEvent& event);
-    std::unique_ptr<wxWebView> m_webView;
-    wxString m_directoryMapping;
-    wxString m_indexPath;
+    wxSharedPtr<wxReactArchive> m_archive;
+    std::unordered_map<wxString, wxRequestHandler> m_requestHandlers;
 };
 
 class wxReactApp : public wxApp
@@ -30,6 +62,28 @@ class wxReactApp : public wxApp
 public:
     virtual bool OnInit() override final;
     virtual bool OnReady() = 0;
+
 private:
     static bool StartProcess();
 };
+
+class wxReactView
+{
+public:
+    wxReactView(wxWindow * parent, wxWindowID id,
+                 const wxPoint &pos = wxDefaultPosition,
+                 const wxSize &size = wxDefaultSize,
+                 long style = 0,
+                 const wxString &name = wxASCII_STR(wxWebViewNameStr));
+
+    void Initialize(wxReactHandler *handler, const wxString &indexPage = "index.html");
+
+    wxWebView* GetWebView() const { return m_webView.get(); }
+
+private:
+    std::unique_ptr<wxWebView> m_webView;
+    wxString m_indexPage;
+    wxSharedPtr<wxWebViewHandler> m_handler;
+};
+
+#endif
